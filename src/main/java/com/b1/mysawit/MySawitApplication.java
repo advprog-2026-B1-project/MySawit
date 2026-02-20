f6fd9ff5b3e9f37b6ff9497b39b21639ffae3e24
Roberto Eugenio Sugiarto <robertotoes12345@gmail.com>
2026-02-20 15:14:05 +0700

    first commit

---
+++ src/main/java/com/b1/mysawit/MySawitApplication.java
@@ -0,0 +1,13 @@
+package com.b1.mysawit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySawitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySawitApplication.class, args);
+    }
+
+}
