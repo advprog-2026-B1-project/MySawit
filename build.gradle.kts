24064f438af55054e2d85865dc7baf52d03d2e2b
Roberto Eugenio Sugiarto <robertotoes12345@gmail.com>
2026-02-20 15:53:02 +0700

    add ci script

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,12 @@
+import org.gradle.testing.jacoco.tasks.JacocoReport
+
 plugins {
     java
-    id("org.springframework.boot") version "4.0.3"
+    id("org.springframework.boot") version "3.4.3"
     id("io.spring.dependency-management") version "1.1.7"
+    id("jacoco")
+    id("checkstyle")
+    id("org.sonarqube") version "7.2.2.6593"
 }
 
 group = "com.b1"
@@ -10,7 +15,33 @@ description = "MySawit"
 
 java {
     toolchain {
-        languageVersion = JavaLanguageVersion.of(21)
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
+}
+
+checkstyle {
+    toolVersion = "10.12.5"
+    isIgnoreFailures = false
+    isShowViolations = true
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "advprog-2026-B1-project_MySawit")
+        property("sonar.organization", "advprog-2026-b1-project")
+        property("sonar.host.url", "https://sonarcloud.io")
+    }
+}
+
+configurations.named("checkstyle") {
+    resolutionStrategy.capabilitiesResolution.withCapability("com.google.collections:google-collections") {
+        select("com.google.guava:guava:0")
+    }
+}
+
+tasks.named<JacocoReport>("jacocoTestReport") {
+    reports {
+        xml.required.set(true)
     }
 }
 
@@ -26,17 +57,21 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
-    implementation("org.springframework.boot:spring-boot-starter-webmvc")
+    implementation("org.springframework.boot:spring-boot-starter-web")
     compileOnly("org.projectlombok:lombok")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     runtimeOnly("org.postgresql:postgresql")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
     annotationProcessor("org.projectlombok:lombok")
-    testImplementation("org.springframework.boot:spring-boot-starter-thymeleaf-test")
-    testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
-tasks.withType<Test> {
+tasks.named<Test>("test") {
     useJUnitPlatform()
+    finalizedBy(tasks.named("jacocoTestReport"))
+}
+
+tasks.named("sonar") {
+    dependsOn(tasks.test)
 }
