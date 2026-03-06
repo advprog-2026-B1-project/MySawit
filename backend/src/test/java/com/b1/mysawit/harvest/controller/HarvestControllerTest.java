0110299263ec803158ab04d7600714fe354c065b
Hannan-Afif <hannanafifd@gmail.com>
2026-03-06 21:24:51 +0700

    removing redundant on import and replacing depricated

--- backend/src/test/java/com/b1/mysawit/harvest/controller/HarvestControllerTest.java
+++ backend/src/test/java/com/b1/mysawit/harvest/controller/HarvestControllerTest.java
@@ -5,12 +5,10 @@ import com.b1.mysawit.harvest.service.HarvestService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.math.BigDecimal;
-import java.time.LocalDate;
 import java.util.List;
 
 import static org.mockito.ArgumentMatchers.any;
@@ -25,7 +23,7 @@ class HarvestControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+    @MockitoBean
     private HarvestService harvestService;
 
     @Test
