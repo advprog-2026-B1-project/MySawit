0b4f9cbba13adedc0a476f7b2dfeb6450632716c
Roberto Eugenio Sugiarto <robertotoes12345@gmail.com>
2026-03-06 16:09:49 +0700

    update new directory system

---
+++ backend/src/test/java/com/b1/mysawit/delivery/MySawitDeliveryTests.java
@@ -0,0 +1,13 @@
+package com.b1.mysawit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySawitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
